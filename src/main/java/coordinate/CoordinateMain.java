25e18f32137406cf58f05f4e4555141f5e8ebdc9
JunYoung Jang <police0023@naver.com>
2023-05-06 16:00:34 +0900

    feat: CoordinateMain

---
+++ src/main/java/coordinate/CoordinateMain.java
@@ -0,0 +1,8 @@
+package coordinate;
+
+import coordinate.controller.CoordinateCalculator;
+public class CoordinateMain {
+    public static void main(String[] args) {
+        new CoordinateCalculator().run();
+    }
+}
